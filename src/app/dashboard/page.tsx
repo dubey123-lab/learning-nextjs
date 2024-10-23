66d377fedf44d6e77c6eb63268d96ca0c9127c78
Tagline <tagline@Taglines-iMac-Pro-97.local>
2024-10-23 18:08:11 +0530

    initial commit

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashBoard = () => {
+  return <div>Welcome To My karkhana Hahhaha .....</div>;
+};
+
+export default DashBoard;
---
+++ src/app/dashboard/setting/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SettingPage = () => {
+  return <div>Bhai Setting Nahi Ho rah hai Karwa de Moye Moye..........</div>;
+};
+
+export default SettingPage;
